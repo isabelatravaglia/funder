e9d70144c3b8bc47d1d301464f40b2e7eeaf30bb
Isabela Santos <isabelatravaglia@gmail.com>
2019-09-02 14:03:57 +0100

    added institution model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_123827) do
+ActiveRecord::Schema.define(version: 2019_09_02_130307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2019_09_02_123827) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "institutions", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "country"
+    t.string "ranking"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_institutions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +44,5 @@ ActiveRecord::Schema.define(version: 2019_09_02_123827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "institutions", "users"
 end
