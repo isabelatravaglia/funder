eab764644d509f6c2670cdad4edc39f38c2ad2af
Isabela Santos <isabelatravaglia@gmail.com>
2019-09-04 17:42:37 +0100

    added background job to check for alert matches in the scholarship model. Added sidekiq and redis to allow background job execution. Added admin column to Users to enable access to Sidekiq dashboard.

---
+++ app/jobs/check_new_scholarships_job.rb
@@ -0,0 +1,22 @@
+class CheckNewScholarshipsJob < ApplicationJob
+  queue_as :default
+
+  def perform(scholarship_id)
+    # Do something later
+    puts "checking if new scholarship corresponds to alerts"
+    @scholarship = Scholarship.find(scholarship_id)
+    @alerts = Alert.where("(area_id = #{@scholarship.area_id} and institution_id = #{@scholarship.institution_id}) or (area_id = #{@scholarship.area_id} and institution_id is null) or (area_id is null and institution_id = #{@scholarship.institution_id})")
+    unless @alerts.empty?
+      puts "alert match!"
+      @alerts.each do |alert|
+        puts "creating new notification for alert #{alert.id}"
+        @notification = Notification.new
+        @notification.alert = alert
+        @notification.status = "unread"
+        @notification.save
+        puts "new notification created!"
+      end
+    end
+    puts "ending alert check"
+  end
+end
---
+++ test/jobs/check_new_scholarships_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CheckNewScholarshipsJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
