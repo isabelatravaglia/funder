3006382bd83a629c6ba61e94d6219e4559a95ed1
Isabela Santos <isabelatravaglia@gmail.com>
2019-09-06 11:06:39 +0100

    changed sorting logic on users controller. Added a logic to count unread notifications on the notification model. On the user show page, changed the logic to order the alerts based on the unread notifications. Added a migration to add the column unread_notif_count on the alerts table.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    @sorted_alerts = current_user.alerts.order("notif_count desc nulls last")
+    @sorted_alerts = current_user.alerts.order("unread_notif_count desc nulls last, created_at desc")
   end
 
   def edit
