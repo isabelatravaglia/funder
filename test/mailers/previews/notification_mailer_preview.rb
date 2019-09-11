9fa39a34ecdbda6078186991cd4af6a96918fdac
Isabela Santos <isabelatravaglia@gmail.com>
2019-09-11 11:23:52 +0100

    changed the user logic to get the notification user

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -3,8 +3,8 @@ class NotificationMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/notification
   def notification
-    user = User.first
     new_notification = user.notifications.where(status:"unread").last
+    user = new_notification.alert.user
     NotificationMailer.with(user: user, new_notification: new_notification).notification.deliver_now
   end
 
