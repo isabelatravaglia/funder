59cd67d422d480c20ad2efb99e7bdd049d4a9bf6
vicente dauce <dauce@ua.pt>
2019-09-11 13:36:10 +0100

    university ranked

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,53 +19,53 @@ admin.save
 puts "Creating institutions"
 
 url_1 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503775/institutions/university_of_toronto_1_1_b3shsz.jpg"
-inst_1 = Institution.new(name: "University of Vienna", city: "Vienna", country: "Austria", ranking: rand(1..50), user: admin )
+inst_1 = Institution.new(name: "University of Vienna", city: "Vienna", country: "Austria", ranking: 63, user: admin )
 inst_1.remote_photo_url = url_1
 inst_1.save
 
 url_2 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503756/institutions/190426-ucla-measles-se-610p_2af8c7a37d7e423cacf6805c7cbc08f0.fit-760w_trohil.jpg"
-inst_2 = Institution.new(name: "University of Debrecen", city: "Budapeste", country: "Hungary", ranking: rand(1..50), user: admin )
+inst_2 = Institution.new(name: "University of Debrecen", city: "Budapeste", country: "Hungary", ranking: 361, user: admin )
 inst_2.remote_photo_url = url_2
 inst_2.save
 
 url_3 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503736/institutions/FN-AI051_fn_uni_M_20190830130204_uceqnf.jpg"
-inst_3 = Institution.new(name: "University of Porto", city: "Porto", country: "Portugal", ranking: rand(1..50), user: admin )
+inst_3 = Institution.new(name: "University of Porto", city: "Porto", country: "Portugal", ranking: 197, user: admin )
 inst_3.remote_photo_url = url_3
 inst_3.save
 
 url_4 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503719/institutions/0943b1554aafbb45a7f771d4a04cd34d02eb6b21cb7e469c73cc22dc8561dcd1_hgjvmv.jpg"
-inst_4 = Institution.new(name: "Oslo University College", city: "Oslo", country: "Norway", ranking: rand(1..50), user: admin )
+inst_4 = Institution.new(name: "Oslo University College", city: "Oslo", country: "Norway", ranking: 49, user: admin )
 inst_4.remote_photo_url = url_4
 inst_4.save
 
 url_5 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503702/institutions/wide_fullhd_russia-moscow-lomonosov-msu_vhj3kk.jpg"
-inst_5 = Institution.new(name: "ETH Zurich", city: "Zurich", country: "Switzerland", ranking: rand(1..50), user: admin )
+inst_5 = Institution.new(name: "ETH Zurich", city: "Zurich", country: "Switzerland", ranking: 37, user: admin )
 inst_5.remote_photo_url = url_5
 inst_5.save
 
 url_6 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503687/institutions/456071_7283734_universities_updates_jwpfmu.jpg"
-inst_6 = Institution.new(name: "University College Copenhagen", city: "Copenhagen", country: "Denmark", ranking: rand(1..50), user: admin )
+inst_6 = Institution.new(name: "University of Copenhagen", city: "Copenhagen", country: "Denmark", ranking: 44, user: admin )
 inst_6.remote_photo_url = url_6
 inst_6.save
 
 url_7 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503674/institutions/national-university-of-ireland-galway-aula-maxima-quadrangle_z7a6ed.jpg"
-inst_7 = Institution.new(name: "University of Warsaw", city: "Warsaw", country: "Poland", ranking: rand(1..50), user: admin )
+inst_7 = Institution.new(name: "University of Warsaw", city: "Warsaw", country: "Poland", ranking: 286, user: admin )
 inst_7.remote_photo_url = url_7
 inst_7.save
 
 
 url_8 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503662/institutions/colleges-universities-using-social-media_xwyat7.jpg"
-inst_8 = Institution.new(name: "Trinity College", city: "Dublin", country: "Ireland", ranking: rand(1..50), user: admin )
+inst_8 = Institution.new(name: "Trinity College", city: "Dublin", country: "Ireland", ranking: 48, user: admin )
 inst_8.remote_photo_url = url_8
 inst_8.save
 
 url_9 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503653/institutions/https_3A_2F_2Fspecials-images.forbesimg.com_2Fdam_2Fimageserve_2F503064668_2F960x0_ehgvem.jpg"
-inst_9 = Institution.new(name: "University of Lisbon", city: "Lisbon", country: "Portugal", ranking: rand(1..50), user: admin )
+inst_9 = Institution.new(name: "University of Lisbon", city: "Lisbon", country: "Portugal", ranking: 251, user: admin )
 inst_9.remote_photo_url = url_9
 inst_9.save
 
 url_10 = "https://res.cloudinary.com/veronicabraga/image/upload/v1567503647/institutions/Corpus-Christi-Oxford_u8ztdh.jpg"
-inst_10 = Institution.new(name: "University of Ghent", city: "Ghent", country: "Belgium", ranking: rand(1..50), user: admin )
+inst_10 = Institution.new(name: "University of Ghent", city: "Ghent", country: "Belgium", ranking: 63, user: admin )
 inst_10.remote_photo_url = url_10
 inst_10.save
 
@@ -75,27 +75,27 @@ inst_11.remote_photo_url = url_11
 inst_11.save
 
 url_12 = "https://res.cloudinary.com/veronicabraga/image/upload/v1568132152/Funder/london_school_of_economics_ybb4ks.jpg"
-inst_12 = Institution.new(name: "LSE - The London School", city: "London", country: "England", ranking: rand(1..50), user: admin )
+inst_12 = Institution.new(name: "LSE - The London School", city: "London", country: "England", ranking: 6, user: admin )
 inst_12.remote_photo_url = url_12
 inst_12.save
 
 url_13 = "https://res.cloudinary.com/veronicabraga/image/upload/v1568132023/Funder/methode_2Fsundaytimes_2Fprod_2Fweb_2Fbin_2F5d7fa3d8-2f79-11e8-8818-e8348c6ff993_tbo9sm.jpg"
-inst_13 = Institution.new(name: "Henley Business School", city: "Henley-on-Thames", country: "England", ranking: rand(1..50), user: admin )
+inst_13 = Institution.new(name: "Henley Business School", city: "Henley-on-Thames", country: "England", ranking: 100, user: admin )
 inst_13.remote_photo_url = url_13
 inst_13.save
 
 url_14 = "https://res.cloudinary.com/veronicabraga/image/upload/v1568133827/Funder/cambridge-university-_hcaoa6.jpg"
-inst_14 = Institution.new(name: "University of Cambridge", city: "Cambridge", country: "England", ranking: rand(1..50), user: admin )
+inst_14 = Institution.new(name: "University of Cambridge", city: "Cambridge", country: "England", ranking: 2, user: admin )
 inst_14.remote_photo_url = url_14
 inst_14.save
 
 url_15 = "https://res.cloudinary.com/veronicabraga/image/upload/v1568134955/Funder/385E-09C4-49D2-A37D-1575_oxjrwj.jpg"
-inst_15 = Institution.new(name: "Aston University", city: "Birmingham", country: "England", ranking: rand(1..50), user: admin )
+inst_15 = Institution.new(name: "Aston University", city: "Birmingham", country: "England", ranking: 174, user: admin )
 inst_15.remote_photo_url = url_15
 inst_15.save
 
 url_16 = "https://res.cloudinary.com/veronicabraga/image/upload/v1568136586/Funder/Universita-Cattolica-banner_tcm76-955503_woch3f.jpg"
-inst_16 = Institution.new(name: "Defap- Graduate School in Public Economics", city: "Milano", country: "Italy", ranking: rand(1..50), user: admin )
+inst_16 = Institution.new(name: "Defap- Graduate School in Public Economics", city: "Milano", country: "Italy", ranking: 401, user: admin )
 inst_16.remote_photo_url = url_16
 inst_16.save
 
